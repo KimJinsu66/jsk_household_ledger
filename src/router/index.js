1aae8c122557badb3d8eca874c555d2fa8de6aae
KimJinsu66 <jinsu6688@gmail.com>
2021-06-26 12:13:30 +0900

    initilize file

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../components/home/home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+
+  },
+]
