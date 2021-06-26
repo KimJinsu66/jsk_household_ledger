c4508acdd340a0ddd8e6da741046a012a985e059
KimJinsu66 <jinsu6688@gmail.com>
2021-06-26 19:39:33 +0900

    router file（코드 읽어야 할 필요 있음)

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,11 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../components/home/home.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import SignUp from '../components/sessions/sign_up.vue'
+import SignIn from '../components/sessions/sign_in.vue'
+import WithDrawalNew from '@/views/withdrawal.vue'
+import WithDrawal from '@/views/withdrawal_index.vue'
 
-Vue.use(VueRouter);
+import firebase from 'firebase'
 
 const routes = [
   {
@@ -11,6 +14,54 @@ const routes = [
     component: Home
   },
   {
-
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
+  {
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/withdrawal/new',
+    name: 'WithDrawalNew',
+    component: WithDrawalNew
+  },
+  {
+    path: '/withdrawal',
+    name: 'WithDrawal',
+    component: WithDrawal
+  }
 ]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  let currentUser = firebase.auth().currentUser
+  if (requiresAuth) {
+    // このルートはログインされているかどうか認証が必要です。
+    // もしされていないならば、ログインページにリダイレクトします。
+    if (!currentUser) {
+      next({
+        path: '/signin',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next();
+    }
+  } else {
+    next(); // next() を常に呼び出すようにしてください!
+  }
+})
+
+export default router
