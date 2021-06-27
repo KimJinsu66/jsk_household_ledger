6ca42c802c1658080af9cd76ba2c891f1544cc57
KimJinsu66 <jinsu6688@gmail.com>
2021-06-27 16:31:56 +0900

    routes 한줄로 변경

--- src/router/index.js
+++ src/router/index.js
@@ -2,48 +2,29 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../components/sessions/sign_up.vue'
 import SignIn from '../components/sessions/sign_in.vue'
-import WithDrawalNew from '@/views/withdrawal.vue'
-import WithDrawal from '@/views/withdrawal_index.vue'
+import WithDrawalForm from '../components/household_ledger/withdrawal_form.vue'
+import WithDrawalTable from '../components/household_ledger/withdrawal_table.vue'
+import WithDrawal from '@/views/withdrawal.vue'
 
 import firebase from 'firebase'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+  { path: '/', name: 'Home', component: Home },
+  { path: '/about', name: 'About', component: () => import('../views/About.vue') },
+  { path: '/signin', name: 'SignIn', component: SignIn },
+  { path: '/signup', name: 'SignUp', component: SignUp },
+  { path: '/withdrawal', name: 'WithDrawal', component: WithDrawal,
+    children: [
+      { path: '',    name: 'WithDrawalTable', component: WithDrawalTable },
+      { path: 'new', name: 'WithDrawalForm',  component: WithDrawalForm  }
+    ] 
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/signin',
-    name: 'SignIn',
-    component: SignIn
-  },
-  {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
-  },
-  {
-    path: '/withdrawal/new',
-    name: 'WithDrawalNew',
-    component: WithDrawalNew
-  },
-  {
-    path: '/withdrawal',
-    name: 'WithDrawal',
-    component: WithDrawal
-  }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
 router.beforeEach((to, from, next) => {
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
