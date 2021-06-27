a856e3761bfbeb21c6af88cfcb09a00961adeb9a
KimJinsu66 <jinsu6688@gmail.com>
2021-06-27 12:57:18 +0900

    navbar추가, scss css프레임워크 추가

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="nav">
+    <navbar></navbar>
+  </div>
+  <router-view/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import Navbar from '@/components/common/navbar.vue';
+import Navbar from './components/common/navbar.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'navbar': Navbar
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
