80d1b8070b653ab1fa1cc0e000db9a49cf707fea
KimJinsu66 <jinsu6688@gmail.com>
2021-06-27 16:16:10 +0900

    router-link /추가, withdrawal 위치 변경
    1. withdrawal => /withdrawal로 변경
    2. 영어 이름 한국어로 변경
    3. a link 밑줄 제거 text-decoration: none;

--- src/components/common/navbar.vue
+++ src/components/common/navbar.vue
@@ -5,15 +5,16 @@
 
     <div class="navbar__links">
       <router-link to="/" class="navbar__link">HOME</router-link>
-      <router-link to="about" class="navbar__link">ABOUT</router-link>
-      <router-link to="signin" class="navbar__link">Sign In</router-link>
-      <router-link to="signup" class="navbar__link">Sign Up</router-link>
-      <router-link to="withdrawal" class="navbar__link">WithDrawal</router-link>
+      <router-link to="/withdrawal" class="navbar__link">출금 내역</router-link>
+      <router-link to="/withdrawal/new" class="navbar__link">출금기록</router-link>
+      <router-link to="/signin" class="navbar__link">로그인</router-link>
+      <router-link to="/signup" class="navbar__link">회원가입</router-link>
+      <router-link to="/about" class="navbar__link">ABOUT</router-link>
     </div>
   </div>
 </template>
 
-<style land="scss">
+<style>
 .navbar {
   width: 100%;
   height: 50px;
@@ -21,5 +22,6 @@
 }
 .navbar__link {
   margin-left: 15px;
+  text-decoration: none;
 }
 </style>
