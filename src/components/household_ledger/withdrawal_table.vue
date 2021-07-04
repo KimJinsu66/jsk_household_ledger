3cc0626a5f12d2fda24b6ed01529feb593a98ce9
KimJinsu66 <jinsu6688@gmail.com>
2021-07-05 01:03:25 +0900

    delete button추가 밑 delete메소드 추가

--- src/components/household_ledger/withdrawal_table.vue
+++ src/components/household_ledger/withdrawal_table.vue
@@ -11,18 +11,28 @@
           <th>날짜</th>
           <th>카테고리</th>
           <th>메모</th>
+          <th>삭제</th>
         </tr>
       </thead>
 
       <tbody class="withdrawal-table__body">
-        <tr v-for="widthDrawal in widthDrawals" :key="widthDrawal.id">
+        <tr v-for="(widthDrawal, index) in widthDrawals" :key="widthDrawal.id">
           <td> {{ widthDrawal.amountOfMoney }} </td>
           <td> {{ widthDrawal.date }}          </td>
           <td> {{ widthDrawal.category }}      </td>
           <td> {{ widthDrawal.memo }}          </td>
+          <td>
+            <button 
+              @click="deleteWidthDrawal(widthDrawal.id, index)" 
+              class="withdrawal-table__delete-button"
+            >
+              Delete
+            </button>
+          </td>
         </tr>
       </tbody>
     </table>
+    
     <nav class="withdrawal-table__pagination">
       <ul>
         <li class="withdrawal-pagination__li">
@@ -55,13 +65,28 @@ export default {
       await this.db.collection('withdrawals').where('email', '==', 'jinsu6688@gmail.com')
         .orderBy('date').get().then((querySnapshot) => {
           this.widthDrawalsLength = querySnapshot.docs.length;
+          console.log(querySnapshot);
 
           for (let i = this.startDataNumber(); i < this.limitDataNumber(querySnapshot.docs.length) ; i++) {
             this.widthDrawals.push(querySnapshot.docs[i].data());
+            this.widthDrawals[i % 10].id = querySnapshot.docs[i].id;
           }
+          console.log(this.widthDrawals);
         }).catch( (error) => {
           console.log(`データの取得に失敗しました (${error})`);
         });
+    },
+    async deleteWidthDrawal (id, index) {
+      if (confirm('정말로 삭제하시겠습니까??')) {
+        await this.db.collection("withdrawals").doc(id).delete().then(() => {
+          this.widthDrawals.splice(index, 1);
+          this.widthDrawalsLength = this.widthDrawalsLength - 1;
+        }).catch((error) => {
+          console.error("Error removing document: ", error);
+        });
+      }
+
+      if(this.widthDrawals.length === 0) location.href(`/withdrawal?page=${this.currentPage - 1}`);
     },
     limitDataNumber (dataLength) {
       const limitNumber = this.currentPage * 10 ;
@@ -84,7 +109,7 @@ export default {
 
       if (lastPageNumber < 2) return [1];
 
-      if (lastPageNumber < 5) {
+      if (lastPageNumber <= 5) {
         for(let i = 2; i < lastPageNumber + 1; i++) {
           pageNumbers.push(i);
         }
@@ -115,7 +140,7 @@ export default {
         'withdrawal-pagination__link__active': true
       };
     }
-  },
+  }
 }
 </script>
 
@@ -147,6 +172,13 @@ export default {
       &__thead {
         background-color: #FCCB5F;
         color: #ffffff;
+      }
+      &__delete-button {
+        padding: 10px 10px;
+        background-color: #ff4500;
+        color: #ffffff;
+        border: none;
+        border-radius: 5px;
       }
     }
     &-pagination {
